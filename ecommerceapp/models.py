51459bb76bbad6cd365ef9c1d51f65126806bf60
darko <ki14.skakun@gmail.com>
2023-11-28 10:53:27 +0200

    product db model

--- ecommerceapp/models.py
+++ ecommerceapp/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Contact(models.Model):
-    name=models.CharField(max_length=50)
-    email=models.EmailField()
-    description=models.CharField(max_length=500)
-    phone_number=models.CharField(max_length=15)
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    description = models.CharField(max_length=500)
+    phone_number = models.CharField(max_length=15)
 
     def __str__(self):
-        return f"Contact(name={self.name}, email={self.email})"
+        return f"{self.name} - {self.email}"
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, default="")
+    subcategory = models.CharField(max_length=50, default="")
+    price = models.IntegerField(default=0)
+    description = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='store/images')
+
+    def __str__(self):
+        return self.product_name
